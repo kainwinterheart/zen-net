1a7ef227a2dc6f9e8873b0401cc16536138db9de
Gennadiy Filatov <gfilatov@cpan.org>
2016-05-08 20:25:57 +0300

    better tinymce

--- app/public/js/apps/fiolentmys.js
+++ app/public/js/apps/fiolentmys.js
@@ -83,6 +83,16 @@ angular.module('zenNetAppsFiolentMysApp', ['ui.bootstrap', 'ui-notification', 'z
     $scope.data = {};
     $scope.page = '';
 
+    $scope.tinymceOptions = {
+        height: 500,
+        plugins: [
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table contextmenu paste code'
+        ],
+        toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link'
+    };
+
     $scope.add_block = function(type, after, data) {
         var id = ++ $scope.serial;
 
